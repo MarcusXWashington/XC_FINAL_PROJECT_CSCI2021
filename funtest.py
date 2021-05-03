c75841c80a678c3a15443a1396b2a298472ac40f
MarcusXWashington <81383579+MarcusXWashington@users.noreply.github.com>
2021-05-03 15:31:14 -0400

    Create funtest.py
    This file tests one of the other paths for the game that is tragic but shows you must complete objectives in certain order and obtain all items before doing certain tasks

---
+++ funtest.py
@@ -0,0 +1,9 @@
+#This file tests one of the other paths for the game that is tragic but shows you must complete objectives in certain order and obtain all items before doing certain tasks
+Start
+Hallway
+Storage
+Grab gear
+Hallway
+Engine
+
+
