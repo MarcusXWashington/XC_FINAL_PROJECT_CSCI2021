8499fbbae6d52589b6792a435291290e245e85b8
MarcusXWashington <81383579+MarcusXWashington@users.noreply.github.com>
2021-05-03 15:36:55 -0400

    Create test.py
    #This file tests the most direct and intended path to victory
    #The main functions of the game are to retrieve the gear from the storage room and the code from oxygen in order to activate the emergency alert system, remove the Impostor and then to engage autopilot and then make it to earth. This set of instructions allows for all of those functions to be tested without having to replay the entire game.
    
    Run this file in the terminal and it will test the correct path and demonstrate the ending of the game.

---
+++ test.py
@@ -0,0 +1,23 @@
+#This file tests the most direct and intended path to victory
+#The main functions of the game are to retrieve the gear from the storage room and the code from oxygen in order to activate the emergency alert system, remove the Impostor and then to engage autopilot and then make it to earth. This set of instructions allows for all of those functions to be tested without having to replay the entire game.
+Start
+Hallway
+Storage
+Grab gear
+Oxygen
+Get code
+2
+Hallway
+Admin
+Activate Emergency Mode
+5699
+Hallway
+Electrical
+Hallway
+Engine
+taser
+Hallway
+Storage
+Navigation
+Autopilot
+2008
